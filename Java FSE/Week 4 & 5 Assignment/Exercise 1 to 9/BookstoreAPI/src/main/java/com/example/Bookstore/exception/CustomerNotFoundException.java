14f3bfd57b05482ecaac73b92623c8af76230093
subha <subhasaha.suribzs@gmail.com>
2024-08-20 23:18:54 +0530

    delete week 4 and 5 folder for creating separate folder of each assignment

--- Java FSE/Week 4 & 5 Assignment/Exercise 1 to 9/BookstoreAPI/src/main/java/com/example/Bookstore/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.Bookstore.exception;
-
-
-public class CustomerNotFoundException extends RuntimeException {
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
-
